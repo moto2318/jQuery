fdd342c3854461862433578a119f09ae666accae
モト2318 <suigekka0101@gmail.com>
2023-11-07 23:12:56 +0900

    注釈の追加

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-$(document).ready(function () {
-   $('body').html('<h1>Hello jQuery!!</h1>');
- });
+$(document).ready(function (){
+   $('body').html('<h1>Hellow jQuery!!</h1>')
+});
+
+
+// ※(document).readyを省略することができる
+// $(function(){
+//   jQueryプログラムの内容
+// });
